45790431369a6fd270cf954c80ae37a51db4bd13
gronaldo44 <98724929+gronaldo44@users.noreply.github.com>
2026-01-16 14:02:07 -0700

    build site for gh-pages

--- qr-generator/vite.config.js
+++ qr-generator/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "Starborn-Academy_QR-Codes", // repo name
+  base: "/Starborn-Academy_QR-Codes/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
 });
